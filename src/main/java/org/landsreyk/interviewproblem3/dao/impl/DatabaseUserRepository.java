243ee708a3748cb9a99cb1b655c5682b1ac8627e
AndrewKozyrev <mr.andrey.kozyrev@gmail.com>
2024-10-27 21:08:11 +0300

    commit #7

--- src/main/java/org/landsreyk/interviewproblem3/dao/impl/DatabaseUserRepository.java
+++ src/main/java/org/landsreyk/interviewproblem3/dao/impl/DatabaseUserRepository.java
@@ -2,11 +2,13 @@ package org.landsreyk.interviewproblem3.dao.impl;
 
 import org.landsreyk.interviewproblem3.dao.UserRepository;
 import org.landsreyk.interviewproblem3.model.User;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Primary
 public interface DatabaseUserRepository extends UserRepository, JpaRepository<User, Long> {
 
     Optional<User> findById(Long id);
--- src/main/java/org/landsreyk/interviewproblem3/service/UserService.java
+++ src/main/java/org/landsreyk/interviewproblem3/service/UserService.java
@@ -9,19 +9,19 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
+@RequiredArgsConstructor
 public class UserService {
 
-    private final UserRepository inMemoryUserRepository;
+    private final UserRepository userRepository;
 
     public void save(User user) {
-        inMemoryUserRepository.save(user);
+        userRepository.save(user);
     }
 
     public void createFriendship(long userId1, long userId2) {
-        User user1 = inMemoryUserRepository.findById(userId1).orElseThrow(() -> new RuntimeException("User not found"));
-        User user2 = inMemoryUserRepository.findById(userId2).orElseThrow(() -> new RuntimeException("User not found"));
+        User user1 = userRepository.findById(userId1).orElseThrow(() -> new RuntimeException("User not found"));
+        User user2 = userRepository.findById(userId2).orElseThrow(() -> new RuntimeException("User not found"));
         if (userId1 == userId2) {
             log.error("A user cannot be added as a friend to himself.");
             return;
@@ -30,24 +30,24 @@ public class UserService {
             user1.getFriends().add(user2);
             user2.getFriends().add(user1);
         }
-        inMemoryUserRepository.save(user1);
-        inMemoryUserRepository.save(user2);
+        userRepository.save(user1);
+        userRepository.save(user2);
     }
 
     public User findById(long l) {
-        return inMemoryUserRepository.findById(l).orElseThrow(() -> new RuntimeException("User not found"));
+        return userRepository.findById(l).orElseThrow(() -> new RuntimeException("User not found"));
     }
 
     public void deleteFriendship(long userId1, long userId2) {
-        User user1 = inMemoryUserRepository.findById(userId1).orElseThrow(() -> new RuntimeException("User not found"));
-        User user2 = inMemoryUserRepository.findById(userId2).orElseThrow(() -> new RuntimeException("User not found"));
-        if(userId1 != userId2) {
+        User user1 = userRepository.findById(userId1).orElseThrow(() -> new RuntimeException("User not found"));
+        User user2 = userRepository.findById(userId2).orElseThrow(() -> new RuntimeException("User not found"));
+        if (userId1 != userId2) {
             user1.getFriends().remove(user2);
             user2.getFriends().remove(user1);
         }
     }
 
     public List<User> findAll() {
-        return inMemoryUserRepository.findAll();
+        return userRepository.findAll();
     }
 }
